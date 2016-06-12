ea109813c6d2f7c13e7b0702c0248c5528c7b9a6
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-06-12 10:11:30 +0200

    Test create many engines

--- demo.pl
+++ demo.pl
@@ -17,3 +17,14 @@ get_answers(E, [H|T]) :-
 	engine_get(E, H), !,
 	get_answers(E, T).
 get_answers(_, []).
+
+test_gc(N) :-
+	forall(between(1, N, _), engine_create(_, _, true)),
+	garbage_collect_atoms.
+
+create_n(N, L) :-
+	length(L, N),
+	maplist(create, L).
+
+create(E) :-
+	engine_create(E, _, true).
